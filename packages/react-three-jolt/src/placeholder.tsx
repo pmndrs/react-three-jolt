f1cd20af3f5a6fecdedc12dec934c4696832a99f
“Isaac <isaac@isaacmason.com>
2024-04-07 13:45:40 +1000

    chore: rollup externals

--- packages/react-three-jolt/src/placeholder.tsx
+++ packages/react-three-jolt/src/placeholder.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import { useFrame } from '@react-three/fiber';
+import React, { useRef } from 'react';
+import * as THREE from 'three';
 
 export const Placeholder = () => {
+  const ref = useRef<THREE.Mesh>(null!);
+
+  useFrame(() => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+  });
+
   return (
-    <mesh>
+    <mesh ref={ref}>
       <boxGeometry />
       <meshNormalMaterial />
     </mesh>
