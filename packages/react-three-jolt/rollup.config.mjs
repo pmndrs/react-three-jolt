ff05f4000544afff65ace983f4fd96781f579d1d
“Isaac <isaac@isaacmason.com>
2024-04-14 13:54:50 +1000

    fix: remove terser for now to fix sourcemaps

--- packages/react-three-jolt/rollup.config.mjs
+++ packages/react-three-jolt/rollup.config.mjs
@@ -1,39 +1,41 @@
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
-import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import path from 'path';
 import filesize from 'rollup-plugin-filesize';
 
+const external = [
+    'jolt-physics',
+    '@react-three/fiber',
+    '@react-three/drei',
+    'three',
+    'react',
+    'react-dom'
+];
+
 export default [
     {
         input: `./src/index.ts`,
-        external: ['@react-three/fiber', 'three', 'react', 'react-dom', 'jolt-physics'],
-        inlineDynamicImports: true, // Add this line
+        external,
         output: [
             {
                 file: `dist/index.mjs`,
-                //dir: 'dist',
                 format: 'es',
                 sourcemap: true,
                 exports: 'named'
             },
             {
                 file: `dist/index.cjs`,
-                //dir: 'dist',
                 format: 'cjs',
                 sourcemap: true,
                 exports: 'named'
             }
         ],
         plugins: [
-            terser(),
             nodeResolve(),
             commonjs(),
             typescript({
-                tsconfig: path.resolve(`tsconfig.json`),
-                sourceMap: true,
-                inlineSources: true
+                tsconfig: path.resolve('tsconfig.json')
             }),
             filesize()
         ],
