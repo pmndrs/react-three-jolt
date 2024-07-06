1ff3c8033e2896c00b47f6d58d197d609c1fbc64
“Isaac <isaac@isaacmason.com>
2024-07-06 16:19:05 +1000

    chore: tweak prettier config

--- apps/examples/vite.config.ts
+++ apps/examples/vite.config.ts
@@ -1,9 +1,9 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		open: true
-	}
+  plugins: [react()],
+  server: {
+    open: true,
+  },
 });
